db995f3d79509005937ea20b8fa9d525e6dc98a9
Moons48 <Smooney48@gmail.com>
2016-03-07 21:45:51 -0500

    Updated with sql commands. Still having problems running storing inputs in the actual sql datbase

--- snippets.py
+++ snippets.py
@@ -1,5 +1,7 @@
 import logging
 import argparse
+import psycopg2
+
 
 """Lets see if this pops up on git"""
 
@@ -9,15 +11,20 @@ logging.basicConfig(filename='snippets.log', level=logging.DEBUG)
 #The level argument sets the level... There are a few different levels.
 
 #This is creating the program skeleton and inserting stubs for each one:
-
+connection = 0
 
 def main():
     """Main function
         WHY DO I NEED THIS???"""
     logging.info("Constructing parser")
+    logging.debug("Connecting to PostgreSQL")
+    global connection
+    connection = psycopg2.connect(database="snippets")
+    logging.debug("Database connection established.")
     parser = argparse.ArgumentParser(description = "Store and retrieve snippets of text")
     
     subparsers = parser.add_subparsers(dest = "command", help = "Available commands")
+    #explain this
     
     #subparser for the put command
     
@@ -25,8 +32,7 @@ def main():
     put_parser = subparsers.add_parser("put", help = "Store a snippet")
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help = "Snippet text")
-  #  get_parser = subparsers.add_parser("get", help = "Retrieve a snippet")
- #  get_parser.add_argument("name", help = "what the hell is this doing?")
+    get_parser = subparsers.add_parser("get", help = "Retrieve a snippet")
     
     arguments = parser.parse_args()
     arguments = vars(arguments)
@@ -34,35 +40,43 @@ def main():
     
     if command == "put":
         name, snippet = put(**arguments)
-        print("Stored {!r} as {!r}".format(snippet, name))
+        print"Stored %s as %s"%(snippet, name)
     elif command == "get":
         snippet = get(**arguments)
-        print("Retrieved snippet: {!r}".format(snippet))
+        print("Retrieved snippet: %s"%(snippet))
         
 
-
-    
-    
 def put(name, snippet):
-    """
-    Store a snippet with an associated name.
-    
-    Returns the name of the snippet"""
+    global connection #Had to make connection global to work
+    logging.info("Storing snippet %s: %s"%(name, snippet))
+    cursor = connection.cursor() #allow us to run sql commands in script
+    command = "insert into snippets values (%s, %s)" #these are sql commands
+    cursor.execute(command, (name, snippet)) #running command to the database
+    #?still not sure about why (name, snippet)???
+    connection.commit()#save the changes to the database
+    logging.debug("Snippet stored successfully.")
+    return name, snippet
     
     logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
     
     return name, snippet
     
 def get(name):
-    """Retrieve the snippet with a given name.
-    
-    If there is no such snippet, return '404: Snippet Not Found'.
-    
-    Returns the snippet.
-    """
-    
+    global connection
+    logging.info("Retrieving snippet %s" %(name))
+    cursor = connection.cursor()
+    command = "select message from snippets where keyword = %s"%(name)
+    cursor.fetchone(command, (name))
+    connection.commit()
+    logging.debug("Snippet retrieved successfully. I hope")
+
+
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+    return name
 
 if __name__ == "__main__":
     main()
+
+
+"""So these are all positional arguments, not option arguments that actually
+	do things like the fib one I did before"""
